8d093b79b71c5f7fb85e556dccb98bdc17363de2
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-10-20 15:41:21 +0330

    client.py edited for test

--- client.py
+++ client.py
@@ -2,14 +2,18 @@ import socket
 import select
 import errno
 
-HEADER_LENGTH = 10
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 4444))
 
-IP = "127.0.0.1"
-PORT = "4444"
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
 
-my_username = input("Username: ")
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
-
-print("jjj")
+# HEADER_LENGTH = 10
+#
+# IP = "127.0.0.1"
+# PORT = "4444"
+#
+# my_username = input("Username: ")
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.connect((IP, PORT))
+# client_socket.setblocking(False)
