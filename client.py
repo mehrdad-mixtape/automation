352e8e1ed4cb443e44d7ae412bb84a11ce8532d8
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-10-20 21:48:09 +0330

    client.py edited for test branch

--- client.py
+++ client.py
@@ -5,8 +5,15 @@ import errno
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 4444))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+full_msg = ''
+while True:
+    msg = s.recv(8) # receive data from server with 1024 byte buffer
+    if msg.decode("utf-8"):
+        break
+    else:
+        full_msg += msg.decode("utf-8")
+
+print(full_msg)
 
 # HEADER_LENGTH = 10
 #
