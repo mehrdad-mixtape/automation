797e6d545230556bf16a2c91cf6ea53ce9e79c62
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-10-20 15:20:06 +0330

    server.py started :)

---
+++ server.py
@@ -0,0 +1,12 @@
+import socket
+#import
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET = create a ipv4 socket |||| socket.SOCK_STREAM = this socket work with TCP-IP
+s.bind((socket.gethostname(), 4444)) # socket.gethostname() = get hostname from client on port number
+s.listen(5) # this socket listen on
+
+while True: # each client want to connect to the server, this part catch requests,
+    client_socket, address = s.accept() # client_socket is obj for client.
+    print(f"Connection from {address} has been established :D")
+    # print("Connection form ",address," has been established :D")
+    client_socket.send(bytes("Welcome to the automation_server :)", "utf-8")) # server said to client welcome.
