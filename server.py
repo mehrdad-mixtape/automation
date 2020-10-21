563d03ad5d5d07f4cfc9bd97bd4e964b3783f190
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-10-21 18:57:56 +0330

    server.py is complete

--- server.py
+++ server.py
@@ -1,13 +1,66 @@
 import socket
+import select
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET = create a ipv4 socket |||| socket.SOCK_STREAM = this socket work with TCP-IP
-s.bind((socket.gethostname(), 4444)) # socket.gethostname() = get hostname from client on port number
-s.listen(5) # this socket listen on 4444
+HEADER_LENGTH = 10
 
-while True: # each client want to connect to the server, this part catch requests,
-    client_socket, address = s.accept() # client_socket is obj for client.
-    print(f"Connection from {address} has been established :D")
-    # print("Connection form ",address," has been established :D")
-    client_socket.send(bytes("Welcome to the automation_server :)", "utf-8")) # server said to client welcome.
-    client_socket.close() # when I send data to client I close this socket
-    break
+IP = "127.0.0.1"
+PORT = 4444
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET = create a ipv4 socket |||| socket.SOCK_STREAM = this socket work with TCP-IP
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_socket.bind((IP, PORT))
+server_socket.listen()
+
+sockets_list = [server_socket]
+clients = {}
+
+def receive_message(client_socket):
+    try:
+        message_header = client_socket.recv(HEADER_LENGTH)
+        if not len(message_header):
+            return False
+
+        message_length = int(message_header.decode('utf-8').strip())
+        return {"header": message_header, "data": client_socket.recv(message_length)}
+
+    except:
+        return False
+
+
+while True:
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+
+            client_socket, client_adderss = server_socket.accept() # client address = IP:PORT
+            user = receive_message(client_socket)
+
+            if user is False:
+               continue
+
+            sockets_list.append(client_socket)
+            clients[client_socket] = user
+
+            print(f"Acceped new conection from {client_adderss[0]}:{client_adderss[1]} username:{user['data'].decode('utf-8')}")
+
+        else:
+            message = receive_message(notified_socket)
+
+            if message is False:
+                print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+                sockets_list.remove(notified_socket)
+                del clients[notified_socket]
+                continue
+
+            user = clients[notified_socket]
+            print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+            for client_socket in clients:
+                if client_socket != notified_socket:
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    for notified_socket in exception_sockets:
+        sockets_list.remove(notified_socket)
+        del clients[notified_socket]
