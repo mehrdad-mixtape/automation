b2bda76b7892ee88894388ae01ee736adc799747
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-11-17 21:07:40 +0330

    ok I ready to connect project to db

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-import socket # use for socket.
-import select # use for many client.
-import pymongo
+import socket, select, pymongo, datetime # use for socket.
 
 class Server():
     def __init__(self):
@@ -15,6 +13,9 @@ class Server():
             self.sockets_list.remove(owner_cmd)
             del self.clients[owner_cmd]
 
+    def Server_time(self):
+        return datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
+
     def Receive_Message(self, client_socket): # internal function
         try:
             message_header = client_socket.recv(self.HEADER_LENGTH) # try to get first message from clients with 10 bytes.
@@ -24,11 +25,11 @@ class Server():
             message_length = int(message_header.decode("utf-8").strip()) # yes my message received XD, I should decode received message to utf-8 and calculate length.
             message_data = client_socket.recv(message_length).decode('utf-8').split(" ") # "username password".split(" ")
             return {"header": message_header, "data": message_data[0]} # we can receive message from each client with new header_length size.
-
         except:
             return False
 
     def Run_Server(self):
+
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET = create a ipv4 socket, socket.SOCK_STREAM = this socket work with TCP-IP.
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # initial socket.
         server_socket.bind((self.IP, self.PORT)) # bind ip and port on socket.
@@ -46,13 +47,13 @@ class Server():
 
                     client_socket, client_address = server_socket.accept() # when client connect to my server I accept it and get IP:PORT from it. I get client_socket and client address=[IP, PORT]
                     user = self.Receive_Message(client_socket) # I call my def to receive message from client. this def return a dictionary that contain message_header and data, data for first time is username of client.
-                    if user is False: # if user don't send message I continue other lines(disconnect client) :).
+                    if user is False: # user can press enter with out message
                         continue
                     # else:
                     self.sockets_list.append(client_socket)
                     self.clients[client_socket] = user # I create dictionary keys=client_socket : value= a dictionary that I received from client.
 
-                    print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data']}") # yes you connected ! and your first message is your username.
+                    print(f"{self.Server_time()} '{user['data']}' with {client_address[0]}:{client_address[1]} login to server") # yes you connected ! and your first message is your username.
 
                 ################################ for second time we check client that send message.################################
                 else:
@@ -65,7 +66,7 @@ class Server():
                         continue
 
                     user = self.clients[notified_socket] # I access to {"header": message_header, "data": client_socket.recv(message_length)}
-                    print(f"message from #{user['data']}# : {message['data']}")
+                    print(f"{self.Server_time()} message from {user['data']}: {message['data']}")
 
                     self.Instraction_Handler(message['data'], notified_socket)
 
