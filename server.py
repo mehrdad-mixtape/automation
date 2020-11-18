860e9d29ea6841e05a50035353227ade7d66fdf9
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-11-18 23:12:51 +0330

    I change receive_massage a little and create 2 function Send_message , Authenticate()

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import socket, select, datetime # use for socket.
+import socket, select, datetime# use for socket.
 import manage_db
 
 class Server():
     def __init__(self):
         self.HEADER_LENGTH = 10 # size packets.
         self.IP = "127.0.0.1"
-        self.PORT = 8888
+        self.PORT = 4444
         self.sockets_list = [] # list of sockets : server and other client.
         self.clients = {} # {socket:data}.
 
@@ -25,17 +25,23 @@ class Server():
 
             message_length = int(message_header.decode("utf-8").strip()) # yes my message received XD, I should decode received message to utf-8 and calculate length.
             message_data = client_socket.recv(message_length).decode('utf-8').split(" ") # "username password".split(" ")
-            return {"header": message_header, "data": message_data[0]} # we can receive message from each client with new header_length size.
+            return {"header": message_header, "data": message_data} # we can receive message from each client with new header_length size.
         except:
             return False
+
+    def Send_Message(self, client_socket, message):
+        pass
+
+    def Authenticate(self):
+        pass
 
     def Run_Server(self):
 
         db = manage_db.Automation_BD()
-        db.Insert_admin('mehrdad', 'arman')
-        #db.Delete_admin("mixtape")
+        #db.Insert_admin('mixtape', str(hashlib.sha256('123'.encode('utf-8')).hexdigest()), 'mehrdad', 'arman', 'mehrdad1998a@gmail.com', '09369798295')
+        #db.Delete_admin("5fb52fc768fdd8977be33228")
         #db.Update_admin("5fb427cb2584ac9a1dd9f933", 'sname', 'mixtape')
-        #db.Get_attrib_admin()
+        #print(db.Get_attrib_admin('mixtape'))
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.AF_INET = create a ipv4 socket, socket.SOCK_STREAM = this socket work with TCP-IP.
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # initial socket.
         server_socket.bind((self.IP, self.PORT)) # bind ip and port on socket.
@@ -52,28 +58,27 @@ class Server():
                 if notified_socket == server_socket: # server socket is always first socket that give me notify because client connect to my server.
 
                     client_socket, client_address = server_socket.accept() # when client connect to my server I accept it and get IP:PORT from it. I get client_socket and client address=[IP, PORT]
-                    user = self.Receive_Message(client_socket) # I call my def to receive message from client. this def return a dictionary that contain message_header and data, data for first time is username of client.
-                    if user is False: # user can press enter with out message
+                    user_pass = self.Receive_Message(client_socket) # I call my def to receive message from client. this def return a dictionary that contain message_header and data, data for first time is username of client.
+                    if user_pass is False: # user can press enter with out message
                         continue
                     # else:
                     self.sockets_list.append(client_socket)
-                    self.clients[client_socket] = user # I create dictionary keys=client_socket : value= a dictionary that I received from client.
+                    self.clients[client_socket] = user_pass # I create dictionary keys=client_socket : value= a dictionary that I received from client.
 
-                    print(f"{self.Server_time()} '{user['data']}' with {client_address[0]}:{client_address[1]} login to server") # yes you connected ! and your first message is your username.
+                    print(f"{self.Server_time()} '{user_pass['data'][0]}' with {client_address[0]}:{client_address[1]} login to server") # yes you connected ! and your first message is your username.
 
                 ################################ for second time we check client that send message.################################
                 else:
                     message = self.Receive_Message(notified_socket) # my notify socket is client_socket.
 
                     if message is False:
-                        print(f"Closed connection from {self.clients[notified_socket]['data']}") # clients={client_socket:{"header": message_header, "data": client_socket.recv(message_length)}, ...}
+                        print(f"Closed connection from '{self.clients[notified_socket]['data'][0]}'") # clients={client_socket:{"header": message_header, "data": client_socket.recv(message_length)}, ...}
                         self.sockets_list.remove(notified_socket)
                         del self.clients[notified_socket]
                         continue
 
                     user = self.clients[notified_socket] # I access to {"header": message_header, "data": client_socket.recv(message_length)}
-                    print(f"{self.Server_time()} message from {user['data']}: {message['data']}")
-
+                    print(f"{self.Server_time()} message from '{user['data'][0]}':  {message['data']}")
                     self.Instraction_Handler(message['data'], notified_socket)
 
             for notified_socket in exception_sockets:
