8242b08cc39159c5618a18b1c53cfd4d79d243dd
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-11-18 23:13:30 +0330

    I chage all function for get good result

--- manage_db.py
+++ manage_db.py
@@ -9,22 +9,34 @@ class Automation_BD:
         self.user_coll = self.automation_db['user']
         self.server_coll = self.automation_db['server']
         self.workspace_coll = self.automation_db['workspace']
-        self.log_coll = self.automation_db['log']
+        self.login_log__coll = self.automation_db['login_log']
+        self.action_log_coll = self.automation_db['action_log']
         self.script_coll = self.automation_db['script']
 
-    def Insert_admin(self, sname, fname):
-        data = {'sname' : sname, 'fname' : fname}
-        return self.admin_coll.insert_one(data).acknowledged
+    def Insert_admin(self, username, password, fname, lname, email, phone):
+        data = { '_id' : username, 'password' : password, 'fname' : fname, 'lname' : lname, 'email' : email, 'phone' : phone, 'permission' : True}
+        if self.admin_coll.insert_one(data).acknowledged == True:
+            return 'New admin created successfully'
+        else:
+            return 'Operation failed, please try again'
 
-    def Delete_admin(self, id):
-        return self.admin_coll.delete_one({'_id' : ObjectId(id)})
+    def Delete_admin(self, username):
+        if self.admin_coll.delete_one({'_id': username}).acknowledged == True:
+            return 'Admin deleted successfully'
+        else:
+            return 'Operation failed, please try again'
 
-    def Update_admin(self, id, attrib, new_value):
+    def Update_admin(self, username, attrib, new_value):
         data = {attrib : new_value}
-        return self.admin_coll.update_one({'_id' : ObjectId(id)}, {"$set": data}).acknowledged
+        if self.admin_coll.update_one({'_id': username}, {"$set": data}).acknowledged == True:
+            return 'Selected index Update'
 
-    def Get_attrib_admin(self, id):
-        return self.admin_coll.find_one({'_id' : ObjectId(id)})
+    def Get_attrib_admin(self, username):
+        data = self.admin_coll.find_one({'_id' : username})
+        result = []
+        for key in data:
+            result.append(data[key])
+        return result
 
     def Close_connection(self):
         self.connection.close()
