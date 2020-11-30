0e486683a1075e2766a65a7936f202b819579f56
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-12-01 00:54:09 +0330

    I just del create_user()

--- normal_user.py
+++ normal_user.py
@@ -7,15 +7,6 @@ class Normal_user():
         C = client.Client()
         return C.Connect_and_authenticate_to_server(server_ip, server_port, username, password, key)
 
-    # def Create_user(self, username, password, fname, lname, email, phone, permission):
-    #     self.username = username
-    #     self.passwoed = password
-    #     self.fname = fname
-    #     self.lname = lname
-    #     self.email = email
-    #     self.phone = phone
-    #     self.permission = permission
-
     def Run_script(self, script_name):
         pass
     def Create_script(self, script_name, path, permission):
