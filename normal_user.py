dc82295610bc3dfbbce42857d75bcb87e5b06414
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-12-07 01:31:40 +0330

    I change a comment :)

--- normal_user.py
+++ normal_user.py
@@ -8,8 +8,6 @@ class Normal_user():
     def Login(self, server_ip, server_port, username, password, key):
         return self.C.Connect_and_authenticate_to_server(server_ip, server_port, username, password, key)
 
-        ########################################## Script management section ############# status: implement 70% ######################
-
     ########################################## User management section ############# status: implement 70% ######################
     def Show_profile(self, username, password):
         report = self.db.Get_attrib_user(username, password) # report[9] = permission
@@ -25,7 +23,7 @@ class Normal_user():
     def Edit_script(self, script_name, path, permission):
         pass
 
-    def Create_script(self, script_name, path, usability,permission):
+    def Create_script(self, script_name, path, usability, permission):
         if permission == True:
             report = self.db.Insert_script(script_name, path, usability)
             return report
@@ -121,3 +119,15 @@ class Normal_user():
     ########################################## SSH management section ############# status: none ######################
     def SSH(self, username, ip_address, permission):
         pass
+
+if __name__ == "__main__":
+    N = Normal_user()
+    # print(N.Find_script('get-interface.py', True))
+    # print(N.Del_script('set-eigrp', True))
+    # print(N.Create_script('set-eigrp.py', "/home/mehrdad/Documents/my-git/automation/automation_scripts/", 'Set eigrp configuration', True))
+    # print(N.Up_script('put-interface', 'script_name', 'put-interface.py', True))
+
+    # print(N.All_login_log(True))
+    # print(N.All_action_log(True))
+    # print(N.Login_log('hour', '22', True))
+    # print(N.Action_log('hour', '22', True))
