c5da09e48cec903f7d5aebaed787e518f972f396
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-12-07 00:38:33 +0330

    I create two file for work space, admin and user

---
+++ admin_workspace.py
@@ -0,0 +1,5 @@
+import admin, normal_user
+
+class Admin_workspace():
+    def __init__(self):
+        self.A = admin.Admin()
--- workspace.py
+++
@@ -1,8 +0,0 @@
-import manage_db
-
-class Workspace():
-    def __init__(self, username):
-        self.username = username
-
-    def Create_log(self):
-        pass
