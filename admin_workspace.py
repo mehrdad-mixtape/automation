c06c4712a9f90f7f4a130d2a0c5e5528f149dca8
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-12-29 20:58:55 +0330

    I don't need them :)

--- admin_workspace.py
+++
@@ -1,5 +0,0 @@
-import admin
-
-class Admin_workspace():
-    def __init__(self):
-        self.admin = admin.Admin()
--- user_workspace.py
+++
@@ -1,5 +0,0 @@
-import normal_user
-
-class User_workspace():
-    def __init__(self):
-        self.user = normal_user.Normal_user()
