dd03ba2ad75fdcf9fed3ce17228c703441616dec
mehrdad-mixtape <mehrdad0011998@gmail.com>
2020-12-30 20:22:43 +0330

    new scripts and new forms

---
+++ automation_scripts/set-eigrp.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import requests
+import urllib3
+import json
+
+
+ip_address = '192.168.119.100'
+username = 'test'
+password = 'test'
+
+def get_token():
+    """
+    Authenticate and get token
+    """
+
+    url = 'https://%s:55443/api/v1/auth/token-services' % ip_address
+    auth = (username, password) 
+    headers = {'Content-Type':'application/json'}
+    response = requests.post(url, auth=auth, headers=headers, verify=False)
+    json_data = json.loads(response.text)
+    token = json_data['token-id']
+    print('We received token: %s' % token)
+    return token
+
+def set_eigrp(token):
+    """
+	Enable EIGRP on CSR 1000V router.
+    """
+    url = 'https://%s:55443/api/v1/routing-svc/eigrp' % (ip_address)
+    headers={ 'Content-Type': 'application/json', 'Accept':'application/json', 'X-auth-token': token}
+
+    payload = {
+	"routing-protocol-id": "100"
+        }
+    url2 = 'https://%s:55443//api/v1/routing-svc/eigrp/100/networks' % (ip_address)
+    payload2 = {
+	"network":"10.10.10.0/24"
+	}
+    response = requests.post(url, headers=headers, json=payload, verify=False)
+    response2 = requests.post(url2, headers=headers, json=payload2, verify=False)
+    print('The router responds with status code: %s ' % response.status_code)
+    print('The router responds2 with status code: %s ' % response2.status_code)
+# Disable unverified HTTPS request warnings.
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Get token.
+token = get_token()
+
+#Call Def
+set_eigrp(token)
